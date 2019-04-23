5d58a06274258990c64c1f88cb6eae763c0076a8
jmaurelbadi <jules.maurel@badi.com>
2019-04-23 19:04:06 +0200

    trying gmail for verif

--- cypress/integration/Test_mailnator.js
+++ cypress/integration/Test_mailnator.js
@@ -1,12 +1,13 @@
-describe.only('Test mailinator', function() {
+describe.only('Test gmail', function() {
 
     it('Simple email registration"', function() {
-        cy.visit('https://www.mailinator.com/')
-        cy.get('#inboxfield').type('vycieelukz').type('{enter}')
+        cy.visit('https://www.google.com')
+        cy.get('#gb_70').click()
+       cy.get('input').type('teamtestbadi@gmail.com')
         // need to find a way to be able to select the mail to get the code
         // mailinator API is not free
         // or find activation code 
-
+        // OR TRY WITH GMAIL
 
     })
 

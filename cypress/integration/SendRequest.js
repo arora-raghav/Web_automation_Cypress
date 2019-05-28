e77ed0bc332aa1b5ba8070f1ac32b79e52e2d7e5
jmaurelbadi <jules.maurel@badi.com>
2019-05-28 18:04:24 +0200

    check inbox with user name - in progress

--- cypress/integration/SendRequest.js
+++ cypress/integration/SendRequest.js
@@ -22,16 +22,41 @@ describe('Seeker sends request', function() {
     //cy.get('.map__point_active > .map__point > .map__price > .map__cost').click()
     cy.contains('Request to chat').click()
     cy.get('.carousel__item_active > .carousel__link > .carousel__block > .carousel__send')
-    
-     cy.contains('Once accepted you\'ll be able to chat with this badi!')
-     cy.contains('Requested room')
+    cy.contains('Once accepted you\'ll be able to chat with this badi!')
+    cy.contains('Requested room')
+
+     //save lister name in the listerName variable
+     cy.get('.human__name').then(($textString) => {
+         const listerName = $textString.text()
+         console.log(listerName)
+     })
      cy.get('.popup__close').click()
 
     })
 
+    // check request is in the inbox
+    // Lister name is not defined because it's inside a previous promise. Needs to be in the same test
+    // or find another way
+
+    it('Check inbox', function () {
+        cy.get('.TopBar__Nav_list > :nth-child(6) > a').click()
+        cy.url().should('include', '/inbox')
+        cy.get('.InboxRow--User__Name').first().should('contain', listerName)
+        .find('div > p')
+        .should('have.text', 'Room enquiry')
+        .find(parent)
+        cy.get('InboxRow--Status')
+        .first()
+        .find('span')
+        .should('contain', 'pending')
+
+
+    })
+
+
     // log out
     it('Logs out', function () {
-        cy.logOut()
+       // cy.logOut()
     })
 
 

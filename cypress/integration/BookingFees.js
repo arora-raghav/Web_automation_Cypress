fe00bdfbc26bccd5ccfc40174d6898796a17d953
jmaurelbadi <jules.maurel@badi.com>
2019-06-05 11:57:58 +0200

    add structure for booking fee testing to come

---
+++ cypress/integration/BookingFees.js
@@ -0,0 +1,7 @@
+// create user profile in fixtures
+// select user and login
+// search for specific listing and send enquiry OR
+// go directly to conversation
+// check booking document
+// check booking fee matches expecting amount
+// check out https://docs.cypress.io/api/commands/fixture.html#Accessing-Fixture-Data
